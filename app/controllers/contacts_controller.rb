f69bef1d60a1233504cd407c00e933244021dca5
neiro <neiro.mail@gmail.com>
2016-03-01 16:37:08 +0300

    Generate contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,8 @@
+# Contact form controller
+class ContactsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'devise/registrations#new'
+  resources :contacts, only: %i(new create)
+
+  root 'contacts#new'
 end
